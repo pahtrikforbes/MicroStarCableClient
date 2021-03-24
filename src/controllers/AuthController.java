5e2169e3d160e39bc35c04cf4e068875e166bb73
Patrick Forbes <patrickjforbes@students.utech.edu.jm>
2021-03-24 11:03:43 -0500

    configured auth controller and changed RegistrationForm to RegistrationView

--- src/controllers/AuthController.java
+++ src/controllers/AuthController.java
@@ -4,74 +4,96 @@ import utils.CustomizedException;
 
 import utils.Role;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import models.User;
+import serverconnector.ServerConnector;
 /*This class will isolate the logic for authentication and updating password.*/
 public class AuthController {
 
 	private UserController userController;
-	private User user;
+	private ObjectOutputStream objectOutStream;
+	private ObjectInputStream objectInStream;
+	private ServerConnector serverConnector;
+	private Socket socket;
+	private String operation;
 	
 	public AuthController() {
 		this.userController = new UserController();
-		this.user = new User();
+		this.serverConnector = new ServerConnector();
+		this.socket = null;
+		this.operation = "";
 	}
 	
 	public boolean login(int userId,String password, Role role) throws CustomizedException {
 		
-	    this.user = this.userController.findById(userId);
 	    boolean loggedIn = false;
-	    if(this.user != null) {
-	    	
-	    	try {
-	    		this.userController.validatePassword(password, this.user.getPassword());
-	    		
-	    		if(this.user.getRole() == role) {
-	    			loggedIn = true;//user logged in successfully
-	    		}else {
-	    			throw new CustomizedException("User is not registered as "+role);
-	    		}
-			} catch (CustomizedException e) {
+	    
+	    operation = "login";
+	    try {
+			socket = this.serverConnector.getSocket();
+			initializeStreams();
+			objectOutStream.writeObject(operation);
+			objectOutStream.writeObject(userId);
+			objectOutStream.writeObject(password);
+			objectOutStream.writeObject(role);
+			
+			try {
+				loggedIn = (boolean)objectInStream.readObject();
+			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				throw new CustomizedException(e.getMessage());
-				
 			}
-	    }else {
-	    	System.out.println("user not found");
-	    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new CustomizedException(e.getMessage());
+		}
 		
 	    return loggedIn;
 	}
 	
 	public boolean updatePassword(int userId,String oldPassword,String newPassword) throws CustomizedException {
 		boolean passwordUpdated = false;
-		 this.user = this.userController.findById(userId);
-		 if(this.user != null) {
-		    	
-		    	try {
-		    		//test if old password matches the user current password
-		    		this.userController.validatePassword(oldPassword, this.user.getPassword());
-					
-		    		//if passwords match, then we can change it
-		    		this.user.setPassword(this.userController.generatePasswordHash(newPassword));
-					passwordUpdated=true;
-					//update user in database
-					this.userController.updateUser(this.user);
-				} catch (CustomizedException e) {
-					// TODO Auto-generated catch block
-					throw new CustomizedException(e.getMessage());
-				}
-	
-		    }else {
-		    	System.out.println("user not found");
-		    	throw new CustomizedException("User not found");
-		    }
+		operation = "updatePassword";
+
+		try {
+			socket = this.serverConnector.getSocket();
+			initializeStreams();
+			objectOutStream.writeObject(operation);
+			objectOutStream.writeObject(userId);
+			objectOutStream.writeObject(oldPassword);
+			objectOutStream.writeObject(newPassword);
+			
+			try {
+				passwordUpdated = (boolean)objectInStream.readObject();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new CustomizedException(e.getMessage());
+			}
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 		return passwordUpdated;
 	}
 	
 	public int register(User user) throws CustomizedException {
 		
 		int userId = -1;
+		
 		try {
 			
 			userId = this.userController.createUser(user);
@@ -82,5 +104,16 @@ public class AuthController {
 		
 		
 		return userId;
+	}
+	
+	private void initializeStreams()  throws CustomizedException  {
+		try {
+			this.objectOutStream = new ObjectOutputStream(socket.getOutputStream());
+			this.objectInStream = new ObjectInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
