2d58da3682bc3e5ddf6592e0f71962054ee725eb
Patrick Forbes <patrickjforbes@students.utech.edu.jm>
2021-04-10 18:59:05 -0500

    Almost completed chat application

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,8 +1,29 @@
 package driver;
 
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import views.ChatView;
+
 public class Driver {
 
 	public static void main(String[] args) {
+		
+		
+		ChatView cv;
+		try {
+			cv = new ChatView();
+			JFrame jf = new JFrame();
+			cv.showForm();
+			jf.add(cv);
+			
+			jf.setVisible(true);
+			jf.setSize(500,700);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
