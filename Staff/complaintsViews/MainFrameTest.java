20f0f7179e71deaf8dfe85582c566685b2e4881f
jihaughton <jumaishihaughton@gmail.com>
2021-04-10 18:56:53 -0600

    Display all responses

--- Staff/complaintsViews/MainFrameTest.java
+++ Staff/complaintsViews/MainFrameTest.java
@@ -27,7 +27,7 @@ public class MainFrameTest extends JFrame implements ActionListener{
 	private JMenu menu, menu2, menu3, menu4;
 	private JButton logoutBtn;
 	private JMenuBar menuBar;
-	private JMenuItem i1,i3,i2;
+	private JMenuItem i1,i3,i2,i4;
 	static private JDesktopPane desktopPane;
 	
 	
@@ -41,6 +41,7 @@ public class MainFrameTest extends JFrame implements ActionListener{
 		i1 = new JMenuItem("Submit a Complaint");
 		i2 = new JMenuItem("Submit a Response");
 		i3 = new JMenuItem("Complaint Action View");
+		i4=new JMenuItem("View all responses");
 		desktopPane = new JDesktopPane();
 	    showForm();
 	}
@@ -64,9 +65,16 @@ public class MainFrameTest extends JFrame implements ActionListener{
 		
 		this.i3.addActionListener(this);
 		
-		System.out.println("test");
+		
+		
+		
+		this.i4.addActionListener(this);
+		
+		
 		this.logoutBtn.addActionListener(this);
 		
+		
+		System.out.println("test");
 	}
 	
 	public void addComponentsToFrame() {
@@ -74,7 +82,7 @@ public class MainFrameTest extends JFrame implements ActionListener{
 		menu2.add(i1);
 		menu2.add(i3);
 		menu3.add(i2);
-
+		menu3.add(i4);
 		menuBar.add(menu);
 		menuBar.add(menu2);	
 		menuBar.add(menu3);
@@ -126,11 +134,23 @@ public class MainFrameTest extends JFrame implements ActionListener{
 		else if (e.getSource() == i2) {
 			System.out.println("\nSubmit Response clicked!");
 			desktopPane.removeAll();
-			addResponse cv = new addResponse();
-			desktopPane.add(cv).setVisible(true);
-			desktopPane.moveToFront(cv);
-			cv.setSize(desktopPane.getWidth(), desktopPane.getHeight());
-			cv.setLocation(0,0);
+			addResponse ar = new addResponse();
+			desktopPane.add(ar).setVisible(true);
+			desktopPane.moveToFront(ar);
+			ar.setSize(desktopPane.getWidth(), desktopPane.getHeight());
+			ar.setLocation(0,0);
+			
+		}
+		else if (e.getSource() == i4) {
+			System.out.println("\nShow all Responses clicked!");
+			desktopPane.removeAll();
+			DisplayAllResponses dar = new DisplayAllResponses();
+			desktopPane.add(dar).setVisible(true);
+			desktopPane.moveToFront(dar);
+			dar.setSize(desktopPane.getWidth(), desktopPane.getHeight());
+			dar.setLocation(0,0);
+			
+			dar.addRowsToJTable();
 			
 		}
 		
