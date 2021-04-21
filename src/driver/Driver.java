e2a9bef1e4822a855e1205c038fffd0aa94f282a
Patrick Forbes <patrickjforbes@students.utech.edu.jm>
2021-04-21 18:18:00 -0500

    added phone number field to registration form

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,10 +1,5 @@
 package driver;
 
-import java.io.IOException;
-
-import javax.swing.JFrame;
-
-import ResponseView.addResponse;
 import models.User;
 import utils.Role;
 import views.*;
@@ -13,24 +8,10 @@ public class Driver {
 
 	public static void main(String[] args) {
 
-		//new SplashScreen();
+		new SplashScreen();
 		
 		//new ParentWindow(Role.REPRESENTATIVE, new User("Patrick","", null, null, null, null));
 		
-		JFrame jf = new JFrame();
-		ChatView cv;
-		try {
-			cv = new ChatView();
-			cv.showForm();
-			jf.add(cv);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		jf.setSize(900,900);
-		
-		
-        jf.setVisible(true);
 		
 	}
 	
