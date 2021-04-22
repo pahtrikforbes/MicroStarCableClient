261752c2722b114386b9c15808bba26f1d859f35
Kareem Steen <steen.kareem@gmail.com>
2021-04-22 01:01:47 -0500

    Merged with theo and Patrick code

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,29 +1,11 @@
 package driver;
 
-import java.io.IOException;
-
-import javax.swing.JFrame;
-
-import views.ChatView;
 import views.SplashScreen;
 
 public class Driver {
 
 	public static void main(String[] args) {
-//		new SplashScreen();
-		
-		ChatView cv;
-		try {
-			cv = new ChatView();
-			JFrame jf = new JFrame();
-			jf.setSize(900,900);
-			jf.setVisible(true);
-			jf.add(cv);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		new SplashScreen();
 		
 	}
 
